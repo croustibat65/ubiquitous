c14ca264c65c92ed7587907b3045779776a8c77d
Unknown <arnaud.gout@imelavi.fr>
2016-11-12 19:59:36 +0100

    added tcp server

--- Test2/App.xaml.cs
+++ Test2/App.xaml.cs
@@ -30,12 +30,15 @@ namespace Test2
     {
         Guid cbEventId;
         Guid cbLastEventId;
+        TcpServer server;
+
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
         public App()
         {
+            this.server = new TcpServer();
             this.cbEventId = Guid.NewGuid();
             this.cbLastEventId = Guid.NewGuid();
             this.InitializeComponent();
