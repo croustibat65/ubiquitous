b3a30315cf30daa9054957c60cd196be1cd332a3
MBP <reMBP@MacBook-Pro.local>
2017-08-19 11:56:01 +0200

    Window of the Mac Client removed (working as a service)

--- Mac_Client/AppDelegate.cs
+++ Mac_Client/AppDelegate.cs
@@ -6,17 +6,17 @@ namespace Mac_Client
     [Register("AppDelegate")]
     public class AppDelegate : NSApplicationDelegate
     {
-        MainWindowController mainWindowController;
+       // MainWindowController mainWindowController;
 
         public AppDelegate()
         {
         }
 
-        public override void DidFinishLaunching(NSNotification notification)
-        {
-            mainWindowController = new MainWindowController();
-            mainWindowController.Window.MakeKeyAndOrderFront(this);
-        }
+        //public override void DidFinishLaunching(NSNotification notification)
+        //{
+        //    mainWindowController = new MainWindowController();
+        //    mainWindowController.Window.MakeKeyAndOrderFront(this);
+        //}
 
         public override void WillTerminate(NSNotification notification)
         {
