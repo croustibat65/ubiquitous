16cc3c558dd107a2f602c95edec831d79ce41043
MBP <reMBP@MacBook-Pro.local>
2017-08-19 13:27:07 +0200

    Removing the main window file on the mac client

--- Mac_Client/MainWindow.designer.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Mac_Client
-{
-    [global::Foundation.Register("MainWindow")]
-    public partial class MainWindow
-    {
-    }
-}
--- Mac_Client/MainWindowController.designer.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Mac_Client
-{
-    [global::Foundation.Register("MainWindowController")]
-    public partial class MainWindowController
-    {
-    }
-}
